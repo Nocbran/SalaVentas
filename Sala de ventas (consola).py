9249775816783a69a6613cdd412affe2a802e987
Brand <vasbran111@gmail.com>
2025-08-22 00:01:15 -0500

    .

--- Sala de ventas (consola).py
+++ Sala de ventas (consola).py
@@ -1,3 +1,26 @@
+class Identificadores():
+    def __init__(self,IdProductos,IdInventario,IdEmpleado,IdCliente,IdProveedor):
+        self.IdProductos = IdProductos
+        self.IdInventario = IdInventario
+        self.IdEmpleado = IdEmpleado
+        self.IdCliente = IdCliente
+        self.IdProveedor = IdProveedor
+
+    def __str__(self):
+        return f"[{self.IdProductos} {self.IdInventario} {self.IdEmpleado} {self.IdCliente} {self.IdProveedor} ]"
+
+
+class producto():
+    def __init__(self,nombre,categoria,precio_costo,precio_venta,stock):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio_costo = precio_costo
+        self.precio_venta = precio_venta
+        self.stock = stock
+
+    def __str__(self):
+        return f"[{self.nombre} {self.categoria} {self.precio_costo} {self.precio_venta} {self.stock}]"
+
 class Productos():
     pass
 class Inventario():
